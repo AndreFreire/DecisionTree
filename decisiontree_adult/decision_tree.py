cbf25afb61330d232f4cc10e97da1a5ed9325541
Andre Freire <and__re@hotmail.com>
2018-06-10 19:50:06 -0300

    validation with 10-fold-crossvalidation

--- decisiontree_adult/decision_tree.py
+++ decisiontree_adult/decision_tree.py
@@ -1,8 +1,10 @@
 import json
 import sys
 
-from decisiontree.id3_algorithm import calculate_total_entropy, calculate_information_gain
+from decisiontree.id3_algorithm import (calculate_total_entropy,
+                                        calculate_information_gain)
 from decisiontree.utils import read_csv_file
+from random import randint
 
 TREE_FILE_NAME = 'id3_tree.json'
 
@@ -11,6 +13,7 @@ SEPARATOR = '__'
 DECISION_INDEX = 14
 POSITIVE_DECISION = '>50K'
 NEGATIVE_DECISION = '<=50K'
+
 
 def _get_attribute_with_max_information_gain(
         attributtes_information_gain_dict
@@ -67,7 +70,8 @@ def train_decision_tree(attributes, lines, entropy, id3_tree={}):
         variations_lines_dict[line[attribute_index]].append(line)
     for attribute_variation, variations_lines in variations_lines_dict.items():
         variation_entropy = calculate_total_entropy(
-            variations_lines, POSITIVE_DECISION, NEGATIVE_DECISION, DECISION_INDEX
+            variations_lines, POSITIVE_DECISION,
+            NEGATIVE_DECISION, DECISION_INDEX
         )
         id3_tree_key = _get_id3_tree_key(
             selected_attribute, attribute_variation
@@ -110,10 +114,22 @@ def test_data(id3_tree, headers, data):
     return test_data(id3_tree[id3_key], headers, data)
 
 
-def run(training, input_file_headers, input_file_data):
+def separate_folds(file_data, fold_number):
+    folds = [[] for x in range(fold_number)]
+    count = 0
+    while len(file_data) > 0:
+        i = randint(0, (len(file_data) - 1))
+        folds[count].append(file_data.pop(i))
+        count += 1
+        if count > (fold_number - 1):
+            count = 0
+    return folds
+
+
+def run(action, input_file_headers, input_file_data, fold_number):
     headers = read_csv_file(input_file_headers)[0]
     file_data = read_csv_file(input_file_data)
-    if training:
+    if action == 'training':
         total_entropy_play_tennis = calculate_total_entropy(
             file_data, POSITIVE_DECISION, NEGATIVE_DECISION,
             DECISION_INDEX
@@ -122,7 +138,8 @@ def run(training, input_file_headers, input_file_data):
             headers, file_data, total_entropy_play_tennis
         )
         save_json_to_file(id3_tree, TREE_FILE_NAME)
-    else:
+
+    if action == 'test':
         id3_tree = read_id3_tree()
         success = 0
         errors = 0
@@ -132,14 +149,47 @@ def run(training, input_file_headers, input_file_data):
                 success += 1
             else:
                 errors += 1
-        print('Total {} Sucessos {} Erros {}'.format(success+errors, success, errors))
+        print('Total {} Sucessos {} Erros {}'.format(
+            success+errors, success, errors)
+        )
 
+    if action == 'validation':
+        folds = separate_folds(file_data, fold_number)
+        for i in range(fold_number):
+            fold_aux = folds.pop(i)
 
+            training_fold = []
+
+            for j in fold_aux:
+                training_fold.append(j)
+
+            total_entropy_play_tennis = calculate_total_entropy(
+                training_fold, POSITIVE_DECISION, NEGATIVE_DECISION,
+                DECISION_INDEX
+            )
+            id3_tree = train_decision_tree(
+                headers, training_fold, total_entropy_play_tennis
+            )
+            success = 0
+            errors = 0
+            for line in fold_aux:
+                tested_data = test_data(id3_tree, headers, line)
+                if tested_data:
+                    success += 1
+                else:
+                    errors += 1
+            print('Total {} Sucessos {} Erros {}'.format(
+                success+errors, success, errors)
+            )
+
+            folds.insert(i, fold_aux)
 
 if __name__ == '__main__':
     if not len(sys.argv) > 2:
         print('Please provide the headers and data file path.')
     input_file_headers_param = sys.argv[1]
     input_file_data_param = sys.argv[2]
-    training_param = len(sys.argv) > 3 and sys.argv[3].lower() == 'true'
-    run(training_param, input_file_headers_param, input_file_data_param)
+    fold_number = int(sys.argv[4])
+    action = sys.argv[3]
+    run(action, input_file_headers_param,
+        input_file_data_param, fold_number)
